a56fc73fb0bc809dc211c79f117f04f08f900080
Luzin7 <lvictor.dutra@gmail.com>
2023-04-25 20:48:14 -0300

    feat(userGeolocationContext.jsx): cria contexto da localização do usuário

---
+++ src/contexts/userGeolocationContext.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const UserGeolocationContext = createContext();
+
+export const UserGeolocationProvider = ({ children }) => {
+	const cachedData = JSON.parse(localStorage.getItem('GEOLOCATION_CACHE_KEY'));
+	const [userGeolocationInfo, setUserGeolocationInfo] = useState(
+		cachedData || {},
+	);
+
+	return (
+		<UserGeolocationContext.Provider
+			value={{
+				userGeolocationInfo,
+				setUserGeolocationInfo,
+			}}
+		>
+			{children}
+		</UserGeolocationContext.Provider>
+	);
+};
+
+UserGeolocationProvider.propTypes = {
+	children: PropTypes.node.isRequired,
+};
