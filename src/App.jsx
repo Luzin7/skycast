c98538ceba30e5675daf493414f4277d5c87f80f
Luzin7 <lvictor.dutra@gmail.com>
2023-04-28 12:15:23 -0300

    refact(App.jsx): melhora estrutura de contextos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import WeatherInfo from './components/WeatherInfo/WeatherInfo';
 import { UserGeolocationProvider } from './contexts/userGeolocationContext';
 import { UserQueryProvider } from './contexts/userQueryContext';
 
 function App() {
 	return (
 		<>
-			<UserGeolocationProvider>
-				<UserQueryProvider>
+			<UserQueryProvider>
+				<UserGeolocationProvider>
 					<Header />
-				</UserQueryProvider>
-			</UserGeolocationProvider>
+				</UserGeolocationProvider>
+				<WeatherInfo />
+			</UserQueryProvider>
 		</>
 	);
 }
