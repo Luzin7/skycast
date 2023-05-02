676512c1a561b5992835d5742784593d9ccf35fa
Luzin7 <lvictor.dutra@gmail.com>
2023-05-01 21:11:13 -0300

    feat(WeatherInfo.jsx): adiciona tempo atual buscado

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -13,7 +13,7 @@ import {
 	BsEye,
 } from 'react-icons/bs';
 import { UserQueryContext } from '../../contexts/userQueryContext';
-import './weatherInfo.scss';
+import './weatherInfo.css';
 
 export default function WeatherInfo() {
 	const { userQuery } = useContext(UserQueryContext);
@@ -33,6 +33,59 @@ export default function WeatherInfo() {
 				visibility,
 				speed,
 			} = userQuery;
+			const foo = document.querySelector('body');
+
+			const warm = 'warm';
+			const cold = 'cold';
+			if (temp > 22) {
+				foo.classList.add(warm);
+			} else {
+				foo.classList.remove(warm);
+			}
+			if (temp < 0) {
+				foo.classList.add(cold);
+			} else {
+				foo.classList.remove(cold);
+			}
+
+			const dropIcon = () => {
+				if (humidity >= 70) {
+					return <BsDropletFill />;
+				}
+				if (humidity >= 30 && humidity < 70) {
+					return <BsDropletHalf />;
+				}
+				if (humidity < 30) {
+					return <BsDroplet />;
+				}
+
+				return null;
+			};
+
+			const timeIcon = () => {
+				const time = icon.substr(36, 1);
+
+				if (time.includes('d')) {
+					return <BsCloudSunFill className="sun_icon" />;
+				}
+				if (time.includes('n')) {
+					return <BsFillCloudMoonFill className="sun_icon" />;
+				}
+
+				return null;
+			};
+
+			const thermometerIcon = () => {
+				if (feels_like > temp) {
+					return <BsThermometerHalf className="sensation_icon" />;
+				}
+				if (feels_like < temp) {
+					return <BsThermometerLow className="sensation_icon" />;
+				}
+
+				return null;
+			};
+
 			return (
 				<div className="weather_info_box">
 					<h2 className="current_weather_city">
@@ -42,8 +95,7 @@ export default function WeatherInfo() {
 					<div className="current_city_weather">
 						<span className="current_weather_icon">
 							{/* USAR ÍCONE DE ACORDO COM O HORÁRIO */}
-							<BsCloudSunFill />
-							<BsFillCloudMoonFill />
+							{timeIcon()}
 						</span>
 						<span className="current_weather_temperature">
 							{`${temp.toFixed(0)}°C`}
@@ -52,6 +104,7 @@ export default function WeatherInfo() {
 					<div className="current_weather_conditions">
 						<div className="current_weather_condition">
 							<img
+								className="condition_icon"
 								src={icon}
 								alt={`Temperatura atual em ${name}, ${country}`}
 							/>
@@ -59,8 +112,8 @@ export default function WeatherInfo() {
 						</div>
 						<div className="current_weather_sensation">
 							{/* ADICIONAR ÍCONE QUE VARIA DE ACORDO COM A SENSAÇÃO */}
-							<BsThermometerHalf />
-							<BsThermometerLow />
+							{thermometerIcon()}
+							{/* <BsThermometerLow /> */}
 							<p className="current_weather_condition_desc">{`Sensação ${feels_like.toFixed(
 								0,
 							)}°C`}</p>
@@ -68,19 +121,25 @@ export default function WeatherInfo() {
 					</div>
 					<ul className="current_weather_extras">
 						<li>
-							<BsWind />
-							<span>{`${speed.toFixed(1)}m/s`}</span>
+							<span className="li_title">Vento:</span>
+							<span className="li_content">
+								{`${speed.toFixed(1)} km/h`} <BsWind />
+							</span>
 						</li>
 						<li>
 							{/* ADD ÍCONE QUE REMETA A HUMIDADE */}
-							<BsDroplet />
-							<BsDropletHalf />
-							<BsDropletFill />
-							<span>{`Humidade: ${humidity}%`}</span>
+							<span className="li_title">Humidade:</span>
+							<span className="li_content">
+								{`${humidity}%`}
+								{dropIcon()}
+							</span>
 						</li>
 						<li>
-							<BsEye />
-							{`Visibilidade: ${visibility / 1000}km`}
+							<span className="li_title">Visibilidade:</span>
+							<span className="li_content">
+								{`${visibility / 1000}km`}
+								<BsEye />
+							</span>
 						</li>
 					</ul>
 				</div>
