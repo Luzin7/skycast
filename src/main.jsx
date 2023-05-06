b74fa9e6dafd6a6fb4f6093b11fc56ec70237ec9
Luzin7 <lvictor.dutra@gmail.com>
2023-05-05 21:28:35 -0300

    refact(main.jsx): passa provider para o app

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/globalStyle.css';
+import { UserQueryProvider } from './contexts/userQueryContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />
+		<UserQueryProvider>
+			<App />
+		</UserQueryProvider>
 	</React.StrictMode>,
 );
