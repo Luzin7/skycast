8eabd95fb0c6fde0a2f1cc85c28b49d6b957ac68
Luzin7 <lvictor.dutra@gmail.com>
2023-05-05 21:31:12 -0300

    feat(Header.jsx): adiciona mudança de tema e trata dados

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable camelcase */
 import React, { useContext, useState, useRef, useEffect } from 'react';
-import { VscColorMode } from 'react-icons/vsc';
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
 import fetchUserGeolocationAndWeatherData from '../../scripts/getUserGeolocation';
 import { UserGeolocationContext } from '../../contexts/userGeolocationContext';
 import { getCurrentWeatherByQuery } from '../../services/currentWeather';
@@ -12,7 +12,15 @@ export default function Header() {
 	const { userGeolocationInfo } = useContext(UserGeolocationContext);
 	const { setUserQuery, setForecastWeather } = useContext(UserQueryContext);
 	const [searchInput, setSearchInput] = useState('');
+	const [isDarkModeActive, setIsDarkModeActive] = useState(false);
 	const delayToFetch = useRef(null);
+
+	const toggleTheme = () => {
+		setIsDarkModeActive(!isDarkModeActive);
+		const body = document.querySelector('body');
+
+		body.classList.toggle('dark-mode');
+	};
 
 	const handleSearchInput = (value) => {
 		setSearchInput(value);
@@ -38,33 +46,61 @@ export default function Header() {
 		});
 	};
 
+	const handleFetchError = (error) => {
+		const inputText = document.querySelector('.input_search');
+		if (error.response) {
+			if (error.response.status === 404) {
+				setUserQuery(null);
+				inputText.placeholder =
+					'Desculpe! Não encontramos o lugar que você procurou.';
+				inputText.value = '';
+			}
+
+			if (error.response.status !== 404) {
+				inputText.placeholder =
+					'Houve algum problema na busca... Tente novamente';
+				inputText.value = '';
+			}
+		}
+	};
+
 	useEffect(() => {
 		if (searchInput) {
 			if (delayToFetch.current) clearInterval(delayToFetch.current);
 
 			delayToFetch.current = setTimeout(() => {
-				getCurrentWeatherByQuery(searchInput).then(
-					({
-						weather: [{ description, icon }],
-						main: { temp, feels_like, humidity },
-						visibility,
-						wind: { speed },
-						sys: { country },
-						name,
-					}) => {
-						setUserQuery({
-							name,
-							country,
-							description,
-							icon: `https://openweathermap.org/img/wn/${icon}@2x.png`,
-							temp,
-							feels_like,
-							humidity,
+				getCurrentWeatherByQuery(searchInput)
+					.then(
+						({
+							weather: [{ description, icon }],
+							main: { temp, feels_like, humidity },
 							visibility,
-							speed,
-						});
-					},
-				);
+							wind: { speed },
+							sys: { country },
+							name,
+							cod,
+						}) => {
+							if (cod === 200) {
+								setUserQuery({
+									name,
+									country,
+									description,
+									icon: `https://openweathermap.org/img/wn/${icon}@2x.png`,
+									temp,
+									feels_like,
+									humidity,
+									visibility,
+									speed,
+									cod,
+								});
+							}
+						},
+					)
+					.catch((error) => {
+						handleFetchError(error);
+						return null;
+					});
+				setForecastWeather(null);
 				getForecastWeather(searchInput).then(({ city: { name }, list }) => {
 					const hourlyForecast = getForecastWeatherByPosition(list, [0]);
 					const forecastForTomorrow = getForecastWeatherByPosition(list, [7]);
@@ -101,11 +137,7 @@ export default function Header() {
 			);
 		}
 
-		return (
-			<>
-				<span>Não temos informações sobre sua localização.</span>
-			</>
-		);
+		return null;
 	};
 
 	fetchUserGeolocationAndWeatherData();
@@ -116,14 +148,18 @@ export default function Header() {
 				<input
 					className="input_search"
 					type="text"
-					placeholder="Busce por uma cidade..."
+					placeholder="Busque por uma cidade..."
 					value={searchInput}
 					onChange={({ target }) => handleSearchInput(target.value)}
 				/>
 				<div className="current_location_info">
 					{showCurrentLocationWeatherInfo()}
 				</div>
-				{/* <VscColorMode /> */}
+				{isDarkModeActive ? (
+					<MdLightMode className="toggleTheme" onClick={() => toggleTheme()} />
+				) : (
+					<MdDarkMode className="toggleTheme" onClick={() => toggleTheme()} />
+				)}
 			</div>
 		</header>
 	);
